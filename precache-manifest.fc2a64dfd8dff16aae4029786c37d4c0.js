1d04c7bfa2588f9b0cf4e2cf8e984080694819be
ramratan21 <ramrinva12@gmail.com>
2020-08-13 13:28:40 +0530

    Updates

---
+++ precache-manifest.fc2a64dfd8dff16aae4029786c37d4c0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dbcddac9b983be149a49d8b07f7eebb",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "86405a4d3b2c2882dd58",
+    "url": "/Portfolio/static/css/main.6505634c.chunk.css"
+  },
+  {
+    "revision": "e0599ce891d255c9d4cf",
+    "url": "/Portfolio/static/js/2.f58b28e0.chunk.js"
+  },
+  {
+    "revision": "bf08a7c8c325c6b8720bbb531d6c7294",
+    "url": "/Portfolio/static/js/2.f58b28e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86405a4d3b2c2882dd58",
+    "url": "/Portfolio/static/js/main.12d7ae4f.chunk.js"
+  },
+  {
+    "revision": "ca2c035086bdc7035ada",
+    "url": "/Portfolio/static/js/runtime-main.f8cbedc0.js"
+  },
+  {
+    "revision": "c9dbc47578b36fc248025c8c2c7e13fc",
+    "url": "/Portfolio/static/media/Designing.c9dbc475.png"
+  },
+  {
+    "revision": "ce0de63fcf4be2fb8872019cb12e9f05",
+    "url": "/Portfolio/static/media/Development.ce0de63f.png"
+  },
+  {
+    "revision": "af540e98f5ce3c5e6d1c4e2fe778e73c",
+    "url": "/Portfolio/static/media/Profile.af540e98.svg"
+  },
+  {
+    "revision": "f85923797840082c1052459def4eb944",
+    "url": "/Portfolio/static/media/Teaching.f8592379.png"
+  },
+  {
+    "revision": "a789655a7d515583b6821f4a9174cde7",
+    "url": "/Portfolio/static/media/back-ground_h.a789655a.jpg"
+  },
+  {
+    "revision": "8e6d0baf1e2459b1927a98b2da089eb5",
+    "url": "/Portfolio/static/media/html.8e6d0baf.png"
+  },
+  {
+    "revision": "76b81eea827b1688b292b0d8bedbdde8",
+    "url": "/Portfolio/static/media/js.76b81eea.png"
+  },
+  {
+    "revision": "8cf99be32078e2d9b438a7138190677f",
+    "url": "/Portfolio/static/media/react_lib.8cf99be3.png"
+  },
+  {
+    "revision": "724ec087c23544feb7cb65354bedeb68",
+    "url": "/Portfolio/static/media/xd.724ec087.png"
+  }
+]);
